8cd5acfae419fd4383a6384a8d228336e652fd69
Jibi Jose <jijose@sapient.com>
2016-05-13 10:34:13 +0530

    Project Base

---
+++ src/main/java/com/jibi/App.java
@@ -0,0 +1,13 @@
+package com.jibi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
